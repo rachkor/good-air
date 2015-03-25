3957a3aec1ffb703ee1df41c5d83cea91d365e9e
rachkor <rachael.korinek@gmail.com>
2015-03-25 16:22:31 -0700

    add sign in/log out links and logic

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,7 @@
 class Trip < ActiveRecord::Base
 	attr_accessible :description, :name, :date
 
+	belongs_to :user
 	has_many :destinations
 
 	validates_presence_of :name
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  attr_accessible :first_name, :last_name, :email, :password, :password_confirmation
+  
+  has_many :trips
 end
